e03fec4c5483e95bddb216936a24ac8809d4efc5
Muhammed Said USLU <msaiduslu@gmail.com>
2023-04-02 23:36:38 +0300

    Added Rourets and make configuration

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import Navbar from "../components/Navbar";
+import PrivateRouter from "./PrivateRouter";
+import About from "../pages/About";
+import Login from "../pages/Login";
+import NewPost from "../pages/NewPost";
+import Register from "../pages/Register";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/new-post" element={<PrivateRouter />}>
+          <Route path="" element={<NewPost />} />
+        </Route>
+        <Route path="about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router-dom";
+
+const PrivateRouter = () => {
+  const currentUser = true;
+
+  return currentUser ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRouter;
