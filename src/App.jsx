15533be8f073f35e5288db4cba2db4f55387f80d
Muhammed Said USLU <msaiduslu@gmail.com>
2023-04-02 16:28:00 +0300

    Added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import AppRouter from "./router/AppRouter";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <>
+      <AppRouter />
+    </>
+  );
 }
 
 export default App;
