87cb68864fa5849ff1ad74e3e44196cbebc489e2
Muhammed Said USLU <msaiduslu@gmail.com>
2023-04-08 01:29:58 +0300

    Created Myposts page

--- src/pages/MyPosts.jsx
+++ src/pages/MyPosts.jsx
@@ -1,7 +1,27 @@
+import { Grid } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
+import PostCard from "../components/PostCard";
+import usePostCall from "../hooks/usePostCall";
+import { useEffect } from "react";
 
 const MyPosts = () => {
-  return <div>MyPosts</div>;
+  const { userId } = useSelector((state) => state.auth);
+  const { userPosts } = useSelector((state) => state.posts);
+  const { getUserPosts } = usePostCall();
+  useEffect(() => {
+    getUserPosts(userId);
+  }, []);
+
+  return (
+    <Grid container spacing={2} marginTop={1} marginBottom={8}>
+      {userPosts.map((post) => (
+        <Grid key={post.id} item xs={12} sm={6} md={4} lg={3}>
+          <PostCard post={post} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default MyPosts;
