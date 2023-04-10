8f6b716a8737770dd148999cac7e62b987345477
Muhammed Said USLU <msaiduslu@gmail.com>
2023-04-11 02:06:35 +0300

    Started to create theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,26 @@ import { Provider } from "react-redux";
 import store, { persistor } from "./store/store";
 import Notification from "./components/Notification";
 import { PersistGate } from "redux-persist/integration/react";
+import { createTheme } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
+
+// export const ThemeOptions = {
+//   palette: {
+//     mode: "dark",
+//     primary: {
+//       main: "#5893df",
+//     },
+//     secondary: {
+//       main: "#2ec5d3",
+//     },
+//     background: {
+//       default: "#192231",
+//       paper: "#24344d",
+//     },
+//   },
+// };
+
+// const theme = createTheme(ThemeOptions);
 
 function App() {
   return (
@@ -10,7 +30,9 @@ function App() {
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor} />
         <Notification />
+        {/* <ThemeProvider theme={theme}> */}
         <AppRouter />
+        {/* </ThemeProvider> */}
       </Provider>
     </>
   );
